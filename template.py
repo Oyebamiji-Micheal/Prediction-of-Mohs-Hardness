a267da2f856b6e02e23807540b80de60ec592278
Oyebamiji-Micheal <oyebamijimicheal10@gmail.com>
2023-11-30 07:46:19 +0100

    Update template - change config.yml to config.yaml

--- template.py
+++ template.py
@@ -18,7 +18,7 @@ list_of_files = [
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
-    "config/config.yml",
+    "config/config.yaml",
     "params.yaml",
     "schema.yaml",
     "main.py",
